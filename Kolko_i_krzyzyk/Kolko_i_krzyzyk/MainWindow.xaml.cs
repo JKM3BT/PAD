74f03e2fc0c482d18ef7c998928c6fac4b51bafe
Jakub Majewski <jakub.majewski@cyfroweszkoly.pl>
2024-03-15 16:01:22 +0100

    project finished

--- Kolko_i_krzyzyk/Kolko_i_krzyzyk/MainWindow.xaml.cs
+++ Kolko_i_krzyzyk/Kolko_i_krzyzyk/MainWindow.xaml.cs
@@ -47,6 +47,134 @@ namespace Kolko_i_krzyzyk
                 button.Background = IsPlayerTurn ? Brushes.DeepSkyBlue : Brushes.Goldenrod;
                 button.Content = IsPlayerTurn ? "O" : "X";
 
+                if (btn0.Content == "X" && btn4.Content == "X" && btn8.Content == "X" && btn12.Content == "X")
+                {
+                    MessageBox.Show("Wygrywa gracz 1");
+                    Close();
+                }
+                if (btn1.Content == "X" && btn5.Content == "X" && btn9.Content == "X" && btn13.Content == "X")
+                {
+                    MessageBox.Show("Wygrywa gracz 1");
+                    Close();
+                }
+                if (btn2.Content == "X" && btn6.Content == "X" && btn10.Content == "X" && btn14.Content == "X")
+                {
+                    MessageBox.Show("Wygrywa gracz 1");
+                    Close();
+                }
+                if (btn3.Content == "X" && btn7.Content == "X" && btn11.Content == "X" && btn15.Content == "X")
+                {
+                    MessageBox.Show("Wygrywa gracz 1");
+                    Close();
+                }
+
+                if (btn0.Content == "X" && btn1.Content == "X" && btn2.Content == "X" && btn3.Content == "X")
+                {
+                    MessageBox.Show("Wygrywa gracz 1");
+                    Close();
+                }
+                if (btn4.Content == "X" && btn5.Content == "X" && btn6.Content == "X" && btn7.Content == "X")
+                {
+                    MessageBox.Show("Wygrywa gracz 1");
+                    Close();
+                }
+                if (btn8.Content == "X" && btn9.Content == "X" && btn10.Content == "X" && btn11.Content == "X")
+                {
+                    MessageBox.Show("Wygrywa gracz 1");
+                    Close();
+                }
+                if (btn12.Content == "X" && btn13.Content == "X" && btn14.Content == "X" && btn15.Content == "X")
+                {
+                    MessageBox.Show("Wygrywa gracz 1");
+                    Close();
+                }
+
+                if (btn0.Content == "X" && btn5.Content == "X" && btn10.Content == "X" && btn15.Content == "X")
+                {
+                    MessageBox.Show("Wygrywa gracz 1");
+                    Close();
+                }
+                if (btn3.Content == "X" && btn6.Content == "X" && btn9.Content == "X" && btn12.Content == "X")
+                {
+                    MessageBox.Show("Wygrywa gracz 1");
+                    Close();
+                }
+
+
+                if (btn0.Content == "X" && btn4.Content == "X" && btn8.Content == "X" && btn12.Content == "X")
+                {
+                    MessageBox.Show("Wygrywa gracz 1");
+                    Close();
+                }
+                if (btn1.Content == "X" && btn5.Content == "X" && btn9.Content == "X" && btn13.Content == "X")
+                {
+                    MessageBox.Show("Wygrywa gracz 1");
+                    Close();
+                }
+                if (btn2.Content == "X" && btn6.Content == "X" && btn10.Content == "X" && btn14.Content == "X")
+                {
+                    MessageBox.Show("Wygrywa gracz 1");
+                    Close();
+                }
+                if (btn3.Content == "X" && btn7.Content == "X" && btn11.Content == "X" && btn15.Content == "X")
+                {
+                    MessageBox.Show("Wygrywa gracz 1");
+                    Close();
+                }
+
+
+                if (btn0.Content == "O" && btn4.Content == "O" && btn8.Content == "O" && btn12.Content == "O")
+                {
+                    MessageBox.Show("Wygrywa gracz 2");
+                    Close();
+                }
+                if (btn1.Content == "O" && btn5.Content == "O" && btn9.Content == "O" && btn13.Content == "O")
+                {
+                    MessageBox.Show("Wygrywa gracz 2");
+                    Close();
+                }
+                if (btn2.Content == "O" && btn6.Content == "O" && btn10.Content == "O" && btn14.Content == "O")
+                {
+                    MessageBox.Show("Wygrywa gracz 2");
+                    Close();
+                }
+                if (btn3.Content == "O" && btn7.Content == "O" && btn11.Content == "O" && btn15.Content == "O")
+                {
+                    MessageBox.Show("Wygrywa gracz 2");
+                    Close();
+                }
+
+                if (btn0.Content == "O" && btn1.Content == "O" && btn2.Content == "O" && btn3.Content == "O")
+                {
+                    MessageBox.Show("Wygrywa gracz 2");
+                    Close();
+                }
+                if (btn4.Content == "O" && btn5.Content == "O" && btn6.Content == "O" && btn7.Content == "O")
+                {
+                    MessageBox.Show("Wygrywa gracz 2");
+                    Close();
+                }
+                if (btn8.Content == "O" && btn9.Content == "O" && btn10.Content == "O" && btn11.Content == "O")
+                {
+                    MessageBox.Show("Wygrywa gracz 2");
+                    Close();
+                }
+                if (btn12.Content == "O" && btn13.Content == "O" && btn14.Content == "O" && btn15.Content == "O")
+                {
+                    MessageBox.Show("Wygrywa gracz 2");
+                    Close();
+                }
+
+                if (btn0.Content == "O" && btn5.Content == "O" && btn10.Content == "O" && btn15.Content == "O")
+                {
+                    MessageBox.Show("Wygrywa gracz 2");
+                    Close();
+                }
+                if (btn3.Content == "O" && btn6.Content == "O" && btn9.Content == "O" && btn12.Content == "O")
+                {
+                    MessageBox.Show("Wygrywa gracz 2");
+                    Close();
+                }
                 IsPlayerTurn = !IsPlayerTurn;
             }
         }
--- Kolko_i_krzyzyk/Kolko_i_krzyzyk/obj/Debug/net6.0-windows/App.g.i.cs
+++ Kolko_i_krzyzyk/Kolko_i_krzyzyk/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Kolko_i_krzyzyk {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.3.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Kolko_i_krzyzyk {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.3.0")]
         public static void Main() {
             Kolko_i_krzyzyk.App app = new Kolko_i_krzyzyk.App();
             app.InitializeComponent();
